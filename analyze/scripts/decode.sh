77e4cd0dee32d9ef726cc0ccf8d36d3a7345faa3
Stéphane Voltz <stef.dev@free.fr>
2015-02-24 21:10:22 +0100

    decode register and bulk write operations

---
+++ analyze/scripts/decode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# convert pcap format into 'parse' intermediate form
+# and decode simple operations in one pass
+#
+tshark -r $1 -x -P -S++++ -V | awk -f parsedecode.awk >$1.tmp
+#
+# apply all scripts for low-level decoding of remaining operations
+#
+for script in \
+bulk_write_data.awk \
+;
+do
+	echo "executing "$script"..."
+	awk -f $script $1.tmp >$1.res
+	mv $1.res $1.tmp
+done
+mv $1.tmp `basename $1 .pcap`.decode
